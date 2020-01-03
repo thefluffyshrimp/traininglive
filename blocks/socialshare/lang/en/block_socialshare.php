593b2813ea882ce347efff91fb2fb796ed512dfb
avinash <avinash.yashco@gmx.com>
2020-01-03 12:20:45 +0530

    moodle367

---
+++ blocks/socialshare/lang/en/block_socialshare.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of SocialShare - https://github.com/sekharjaji/moodle-block_socialshare
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * socialshare block class.
+ *
+ * @package   block_socialshare
+ * @copyright 2015 onwards Sekhar Jajimoggala
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'SocialShare';
+$string['socialshare'] = 'Social share';
+$string['socialshare:addinstance'] = 'Add Social Share block';
+$string['socialshare:myaddinstance'] = 'Add Social Share block to the My Moodle page';
+
+$string['enablefacebook'] = 'Allow Facebook sharing';
+$string['enabletwitter'] = 'Allow Twitter sharing';
+$string['enablegoogleplus'] = 'Allow Google+ sharing';
+$string['enablestumbleupon'] = 'Allow StumbleUpon sharing';
+
+$string['layout_horizontal'] = 'Horizontal';
+$string['layout_vertical'] = 'Vertical';
+$string['layout'] = 'Layout';
+
+$string['urlscope'] = 'Sharing scope';
+$string['currentpage'] = 'Share current page';
+$string['currentroot'] = 'Share root site';
+$string['currentcourse'] = 'Share current course';
+$string['currentmodule'] = 'Share current module';
+
