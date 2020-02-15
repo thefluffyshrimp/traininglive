7914a33da82fffbd3372e8520d0f59cc5b4dc108
avinash <avinash.yashco@gmx.com>
2020-02-15 13:31:33 +0530

    moodle3.81update

--- admin/tool/ally/vue/comps/td-HTML/td-HTML.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div v-html="value"></div>
-</template>
-<script>
-export default {
-    props: ['value'],
-}
-</script>
--- enrol/joomdle/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version  = 2010073103;
-$plugin->requires = 2010090501;   // Requires this Moodle version
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.0.6";
-$plugin->component = 'enrol_joomdle';
