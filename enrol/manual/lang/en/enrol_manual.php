3c6e34002022c3f08d8bd2996f1deb7ebfa5c8e9
avinash <avinash.yashco@gmx.com>
2020-02-24 17:15:03 +0530

    moodle3.81update

--- enrol/manual/lang/en/enrol_manual.php
+++ enrol/manual/lang/en/enrol_manual.php
@@ -31,42 +31,42 @@ $string['assignroles'] = 'Assign roles';
 $string['browseusers'] = 'Browse users';
 $string['browsecohorts'] = 'Browse cohorts';
 $string['confirmbulkdeleteenrolment'] = 'Are you sure you want to delete these user enrolments?';
-$string['defaultstart'] = 'Default enrolment start';
-$string['defaultperiod'] = 'Default enrolment duration';
-$string['defaultperiod_desc'] = 'Default length of time that the enrolment is valid. If set to zero, the enrolment duration will be unlimited by default.';
-$string['defaultperiod_help'] = 'Default length of time that the enrolment is valid, starting with the moment the user is enrolled. If disabled, the enrolment duration will be unlimited by default.';
+$string['defaultstart'] = 'Default enrollment start';
+$string['defaultperiod'] = 'Default enrollment duration';
+$string['defaultperiod_desc'] = 'Default length of time that the enrollment is valid. If set to zero, the enrollment duration will be unlimited by default.';
+$string['defaultperiod_help'] = 'Default length of time that the enrollment is valid, starting with the moment the user is enrolled. If disabled, the enrollment duration will be unlimited by default.';
 $string['deleteselectedusers'] = 'Delete selected user enrolments';
 $string['editselectedusers'] = 'Edit selected user enrolments';
 $string['enrolledincourserole'] = 'Enrolled in "{$a->course}" as "{$a->role}"';
-$string['enrolusers'] = 'Enrol users';
-$string['enroluserscohorts'] = 'Enrol selected users and cohorts';
-$string['expiredaction'] = 'Enrolment expiry action';
-$string['expiredaction_help'] = 'Select action to carry out when user enrolment expires. Please note that some user data and settings are purged from course during course unenrolment.';
-$string['expirymessageenrollersubject'] = 'Enrolment expiry notification';
-$string['expirymessageenrollerbody'] = 'Enrolment in the course \'{$a->course}\' will expire within the next {$a->threshold} for the following users:
+$string['enrolusers'] = 'Enroll users';
+$string['enroluserscohorts'] = 'Enroll selected users and cohorts';
+$string['expiredaction'] = 'Enrollment expiry action';
+$string['expiredaction_help'] = 'Select action to carry out when user enrollment expires. Please note that some user data and settings are purged from course during course unenrolment.';
+$string['expirymessageenrollersubject'] = 'Enrollment expiry notification';
+$string['expirymessageenrollerbody'] = 'Enrollment in the course \'{$a->course}\' will expire within the next {$a->threshold} for the following users:
 
 {$a->users}
 
-To extend their enrolment, go to {$a->extendurl}';
-$string['expirymessageenrolledsubject'] = 'Enrolment expiry notification';
+To extend their enrollment, go to {$a->extendurl}';
+$string['expirymessageenrolledsubject'] = 'Enrollment expiry notification';
 $string['expirymessageenrolledbody'] = 'Dear {$a->user},
 
-This is a notification that your enrolment in the course \'{$a->course}\' is due to expire on {$a->timeend}.
+This is a notification that your enrollment in the course \'{$a->course}\' is due to expire on {$a->timeend}.
 
 If you need help, please contact {$a->enroller}.';
-$string['manual:config'] = 'Configure manual enrol instances';
-$string['manual:enrol'] = 'Enrol users';
+$string['manual:config'] = 'Configure manual enroll instances';
+$string['manual:enroll'] = 'Enroll users';
 $string['manual:manage'] = 'Manage user enrolments';
 $string['manual:unenrol'] = 'Unenrol users from the course';
 $string['manual:unenrolself'] = 'Unenrol self from the course';
-$string['messageprovider:expiry_notification'] = 'Manual enrolment expiry notifications';
+$string['messageprovider:expiry_notification'] = 'Manual enrollment expiry notifications';
 $string['now'] = 'Now';
 $string['pluginname'] = 'Manual enrolments';
-$string['pluginname_desc'] = 'The manual enrolments plugin allows users to be enrolled manually via a link in the course administration settings, by a user with appropriate permissions such as a teacher. The plugin should normally be enabled, since certain other enrolment plugins, such as self enrolment, require it.';
+$string['pluginname_desc'] = 'The manual enrolments plugin allows users to be enrolled manually via a link in the course administration settings, by a user with appropriate permissions such as a teacher. The plugin should normally be enabled, since certain other enrollment plugins, such as self enrollment, require it.';
 $string['selection'] = 'Selection';
 $string['selectusers'] = 'Select users';
 $string['selectcohorts'] = 'Select cohorts';
-$string['sendexpirynotificationstask'] = "Manual enrolment send expiry notifications task";
+$string['sendexpirynotificationstask'] = "Manual enrollment send expiry notifications task";
 $string['status'] = 'Enable manual enrolments';
 $string['status_desc'] = 'Allow course access of internally enrolled users. This should be kept enabled in most cases.';
 $string['status_help'] = 'This setting determines whether users can be enrolled manually, via a link in the course administration settings, by a user with appropriate permissions such as a teacher.';
@@ -78,8 +78,8 @@ $string['unenrolselectedusers'] = 'Unenrol selected users';
 $string['unenrolselfconfirm'] = 'Do you really want to unenrol yourself from course "{$a}"?';
 $string['unenroluser'] = 'Do you really want to unenrol "{$a->user}" from course "{$a->course}"?';
 $string['unenrolusers'] = 'Unenrol users';
-$string['wscannotenrol'] = 'Plugin instance cannot manually enrol a user in the course id = {$a->courseid}';
-$string['wsnoinstance'] = 'Manual enrolment plugin instance doesn\'t exist or is disabled for the course (id = {$a->courseid})';
+$string['wscannotenrol'] = 'Plugin instance cannot manually enroll a user in the course id = {$a->courseid}';
+$string['wsnoinstance'] = 'Manual enrollment plugin instance doesn\'t exist or is disabled for the course (id = {$a->courseid})';
 $string['wsusercannotassign'] = 'You don\'t have the permission to assign this role ({$a->roleid}) to this user ({$a->userid}) in this course({$a->courseid}).';
 $string['manualpluginnotinstalled'] = 'The "Manual" plugin has not yet been installed';
 $string['privacy:metadata'] = 'The Manual enrolments plugin does not store any personal data.';
